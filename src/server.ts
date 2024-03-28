cfe931761a8ef559e07c99f856e4f898d021209b
edsonpag <edsonsilvapagani@gmail.com>
2024-03-28 20:58:48 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,9 @@ import routes from './routes'
 const app: Express = express()
 const port = 8080
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://buenasalud.fun/'
+}))
 app.use(express.json())
 app.use(useragent.express())
 app.use('/', routes)
