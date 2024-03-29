47b066fa819dfb994914ca9a3ba8e483ec9c6840
edsonpag <edsonsilvapagani@gmail.com>
2024-03-28 21:09:37 -0300

    adicionado block de origin

---
+++ src/middlewares/originBlock.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+
+export function originBlock(req: Request, res: Response, next: NextFunction) {
+    const allowedOrigins = ['https://buenasalud.fun/es-2/']
+    const origin = req.headers.origin
+    console.log(origin)
+    if (origin && allowedOrigins.includes(origin))
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    else
+        return res.status(404).send('Erro desconhecido');
+    next();
+}
--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,13 @@ import express, { Express } from 'express'
 import cors from 'cors'
 import useragent from 'express-useragent'
 import routes from './routes'
+import { originBlock } from './middlewares/originBlock'
 
 const app: Express = express()
 const port = 8080
 
-app.use(cors({
-    origin: 'https://buenasalud.fun/'
-}))
+app.use(originBlock)
+app.use(cors())
 app.use(express.json())
 app.use(useragent.express())
 app.use('/', routes)
