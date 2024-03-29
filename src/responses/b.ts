18abe49be763fa23906d734e7116657ec72ee549
edsonpag <edsonsilvapagani@gmail.com>
2024-03-29 03:11:13 -0300

    ids

--- src/responses/b.ts
+++ src/responses/b.ts
@@ -3,5 +3,5 @@ import { CloackerResponse } from "../interfaces/cloackerResponse.interface";
 export const responseB: CloackerResponse = {
     headline: `<h2>Descubra cómo un simple truco con la piña hizo que mi marido recuperara la visión</h2>`,
     tituloPagina: 'Truco de Piña',
-    vturb: `<div id="vid_6605fd1c3aea810007af09ff" style="position:relative;width:100%;padding: 56.25% 0 0;"> <img id="thumb_6605fd1c3aea810007af09ff" src="https://images.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/6605fd1c3aea810007af09ff/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"> <div id="backdrop_6605fd1c3aea810007af09ff" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div> </div> <script type="text/javascript" id="scr_6605fd1c3aea810007af09ff"> var s=document.createElement("script"); s.src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/6605fd1c3aea810007af09ff/player.js", s.async=!0,document.head.appendChild(s); </script>`,
+    vturb: `660378d8995c33000904c331`,
 }
