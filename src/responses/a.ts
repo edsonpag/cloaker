a204acbedfef3cfcf848549e5129e14b6bbaeb1e
edsonpag <edsonsilvapagani@gmail.com>
2024-04-02 17:52:04 -0300

    ok

--- src/responses/a.ts
+++ src/responses/a.ts
@@ -3,6 +3,5 @@ import { CloackerResponse } from "../interfaces/cloackerResponse.interface";
 export const responseA: CloackerResponse = {
     headline: `<h2>Solo estos <span class="green-highlight">3 ingredientes</span> en agua durante 2 días, y me desperté
     sin ningún problema en la vista... <span class="green-highlight">(¡mira abajo!)</span></h2>`,
-    tituloPagina: 'Buena Salud',
     vturb: `<script src="https://cdn.converteai.net/lib/js/smartplayer/v1/sdk.min.js" data-id="660378d8995c33000904c331"></script> <div id="ifr_660378d8995c33000904c331_wrapper" style="margin: 0 auto; width: 100%"> <div style="padding:56.25% 0 0 0;position:relative;" id="ifr_660378d8995c33000904c331_aspect"> <iframe frameborder="0" allowfullscreen src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/660378d8995c33000904c331/embed.html" id="ifr_660378d8995c33000904c331" style="position:absolute;top:0;left:0;width:100%;height:100%;" referrerpolicy="origin"></iframe> </div> </div>`
 }
