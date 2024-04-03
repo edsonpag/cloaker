fa6cddc8c92964a644fde3a40bab68f7deb665e6
edsonpag <edsonsilvapagani@gmail.com>
2024-04-03 16:34:30 -0300

    ok

--- src/controllers/cloackerController.ts
+++ src/controllers/cloackerController.ts
@@ -3,13 +3,13 @@ import { CloackerUtils } from "../utils/cloackerUtils";
 
 export class CloackerController {
 
-    main(req: Request, res: Response) {
+    async main(req: Request, res: Response) {
         const cloackerUtils = new CloackerUtils()
         cloackerUtils.validarDispositivoMobile(req)
         cloackerUtils.validarParametrosDaUrl(req)
         cloackerUtils.validarReferencia(req)
         cloackerUtils.validarIdiomasPermitidos(req)
-        cloackerUtils.verificaPais(req)
+        await cloackerUtils.verificaPais(req)
         cloackerUtils.montarResposta(res)
         cloackerUtils.salvarFirebase()
         return
