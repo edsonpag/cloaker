cf9d77e7935eed4b24c13a4a473a4d37e5d230af
edsonpag <edsonsilvapagani@gmail.com>
2024-04-06 18:56:38 -0300

    ajustado o retorno

--- src/responses/a2.ts
+++ src/responses/a2.ts
@@ -5,5 +5,6 @@ export const responseA2: CloackerResponse = {
     sin ningún problema en la vista... <span class="green-highlight">(¡mira abajo!)</span></h2>`,
     vturb: `<div id="vid_6611a20f2aa8f900081262d6" style="position:relative;width:100%;padding: 56.25% 0 0;"> <img id="thumb_6611a20f2aa8f900081262d6" src="https://images.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/6611a20f2aa8f900081262d6/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"> <div id="backdrop_6611a20f2aa8f900081262d6" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div> </div>`,
     vturbScriptId: 'scr_6611a20f2aa8f900081262d6',
-    vturbScriptSrc: 'var s=document.createElement("script"); s.src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/6611a20f2aa8f900081262d6/player.js", s.async=!0,document.head.appendChild(s);'
+    vturbScriptSrc: 'var s=document.createElement("script"); s.src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/6611a20f2aa8f900081262d6/player.js", s.async=!0,document.head.appendChild(s);',
+    customScript: 'function carregarComentarios() { var xhr = new XMLHttpRequest(); xhr.onreadystatechange = function () { if (xhr.readyState === 4 && xhr.status === 200) { document.getElementById("comentarios-facebook").innerHTML = xhr.responseText } }; xhr.open("GET", "facebook-comentarios.html", true); xhr.send() } window.onload = function () { carregarComentarios() }'
 }
