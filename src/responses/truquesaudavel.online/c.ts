5fc66ef309fdeb83b164caf17bb9997872b57a92
edsonpag <edsonsilvapagani@gmail.com>
2024-04-13 23:05:45 -0300

    cloakers

--- src/responses/truquesaudavel.online/c.ts
+++ src/responses/truquesaudavel.online/c.ts
@@ -1,10 +1,10 @@
 import { CloackerResponse } from "../../interfaces/cloackerResponse.interface";
 
 export const responseCTruqueSaudavel: CloackerResponse = {
-    headline: `Teste C`,
-    vturb: `<div id="vid_65e3d6a624757e0008559196" style="position:relative;width:100%;padding: 56.25% 0 0;"> <img id="thumb_65e3d6a624757e0008559196" src="https://images.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/65e3d6a624757e0008559196/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"> <div id="backdrop_65e3d6a624757e0008559196" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div> </div>`,
-    vturbScriptId: 'scr_65e3d6a624757e0008559196',
-    vturbScriptSrc: 'var s=document.createElement("script"); s.src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/65e3d6a624757e0008559196/player.js", s.async=!0,document.head.appendChild(s);',
+    headline: `<h2>"Essa bebida caseira com banana me fez <span class="green-highlight">perder 7kg em 21 dias</span>"</h2>`,
+    vturb: `<div id="vid_661b38d1526c7e0008966f78" style="position:relative;width:100%;padding: 56.25% 0 0;"> <img id="thumb_661b38d1526c7e0008966f78" src="https://images.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/661b38d1526c7e0008966f78/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"> <div id="backdrop_661b38d1526c7e0008966f78" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div> </div>`,
+    vturbScriptId: 'scr_661b38d1526c7e0008966f78',
+    vturbScriptSrc: 'var s=document.createElement("script"); s.src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/661b38d1526c7e0008966f78/player.js", s.async=!0,document.head.appendChild(s);',
     customScript: `(function () {
         let customScript = true
     })()`
