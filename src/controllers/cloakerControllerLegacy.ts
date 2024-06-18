7767301309ede419d3c5044a21a46cffa92988f1
edsonpag <edsonsilvapagani@gmail.com>
2024-06-18 15:16:37 -0300

    ok

--- src/controllers/cloakerControllerLegacy.ts
+++ src/controllers/cloakerControllerLegacy.ts
@@ -5,6 +5,8 @@ import { responseCTruqueNaturalSaudavel } from "../responses/truquenaturalsaudav
 import { responseBTruqueNaturalSaudavel } from "../responses/truquenaturalsaudavel.online/b";
 import { responseABuenaSalud } from "../responses/buena-salud.online/a";
 import { responseBBuenaSalud } from "../responses/buena-salud.online/b";
+import { responseAViverBemComSaude } from "../responses/viverbemcomsaude.online/a";
+import { responseBViverBemComSaude } from "../responses/viverbemcomsaude.online/b";
 
 export class CloackerControllerLegacy {
 
@@ -58,6 +60,31 @@ export class CloackerControllerLegacy {
             res.json(responseABuenaSalud)
         else if (cloackerUtils.errors.length > 0)
             res.json(responseBBuenaSalud)
+        return
+    }
+
+    async viverBemComSaude(req: Request, res: Response) {
+        const cloackerUtils = new CloackerUtilsLegacy({
+            validarMobile: true,
+            validarParametrosDaUrl: true,
+            parametroParaValidar: 'c22-bac512',
+            validarReferencia: false,
+            referenciasPermitidas: [],
+            validarIdiomasDoNavegador: false,
+            idiomasBloqueados: [],
+            validarIp: true,
+            paisesBloqueados: ['BR'],
+            utilizarDoisCloacker: false
+        })
+        cloackerUtils.validarDispositivoMobile(req)
+        cloackerUtils.validarParametrosDaUrl(req)
+        cloackerUtils.validarIdiomasPermitidos(req)
+        await cloackerUtils.verificaIp(req)
+        cloackerUtils.salvarFirebase()
+        if (cloackerUtils.errors.length === 0)
+            res.json(responseAViverBemComSaude)
+        else if (cloackerUtils.errors.length > 0)
+            res.json(responseBViverBemComSaude)
         return
     }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -7,5 +7,6 @@ const router = Router()
 router.post('/truqueSaudavel', new CloackerController().truqueSaudavel)
 router.post('/legacy/truqueNaturalSaudavel', new CloackerControllerLegacy().truqueNaturalSaudavel)
 router.post('/legacy/trucosNaturales', new CloackerControllerLegacy().trucosNaturales)
+router.post('/legacy/viverBemComSaude', new CloackerControllerLegacy().trucosNaturales)
 
 export default router
