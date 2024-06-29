9dc9b01c5f265aca04d4826a899de4e7af5a4b3d
edsonpag <edsonsilvapagani@gmail.com>
2024-06-29 02:03:57 -0300

    ok

--- src/responses/viverbemcomsaude.online/b.ts
+++ src/responses/viverbemcomsaude.online/b.ts
@@ -2,9 +2,9 @@ import { CloackerResponseLegacy } from "../../interfaces/cloakerResponseLegacy.i
 
 export const responseBViverBemComSaude: CloackerResponseLegacy = {
     headline: `<h2>Découvrez comment cette simple astuce naturelle peut améliorer votre vision</h2>`,
-    vturb: `<div id="vid_667ef7fa58521d000b183c56" style="position:relative;width:100%;padding: 56.20608899297424% 0 0;"> <img id="thumb_667ef7fa58521d000b183c56" src="https://images.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/667ef7fa58521d000b183c56/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"> <div id="backdrop_667ef7fa58521d000b183c56" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div> </div>  <style> .elementor-element:has(#smartplayer) { width: 100%; } </style>`,
-    vturbScriptId: 'scr_667ef7fa58521d000b183c56',
-    vturbScriptSrc: 'var s=document.createElement("script"); s.src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/667ef7fa58521d000b183c56/player.js", s.async=!0,document.head.appendChild(s);',
+    vturb: `<div id="vid_667f94b2f64a66000b1ec2fc" style="position:relative;width:100%;padding: 56.20608899297424% 0 0;"> <img id="thumb_667f94b2f64a66000b1ec2fc" src="https://images.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/667f94b2f64a66000b1ec2fc/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"> <div id="backdrop_667f94b2f64a66000b1ec2fc" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div> </div>  <style> .elementor-element:has(#smartplayer) { width: 100%; } </style>`,
+    vturbScriptId: 'scr_667f94b2f64a66000b1ec2fc',
+    vturbScriptSrc: 'var s=document.createElement("script"); s.src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/667f94b2f64a66000b1ec2fc/player.js", s.async=!0,document.head.appendChild(s);',
     customScript: `(function () {
         let customScript = true
     })()`,
