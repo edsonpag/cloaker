215bdf1013ca99f3b68f5769e8a73a4187105dad
edsonpag <edsonsilvapagani@gmail.com>
2024-06-29 22:00:19 -0300

    ok

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,6 @@ import { CloackerControllerLegacy } from './controllers/cloakerControllerLegacy'
 
 const router = Router()
 
-router.get('/', (req, res) => res.send("HELLO WORLD"))
 router.post('/truqueSaudavel', new CloackerController().truqueSaudavel)
 router.post('/legacy/viverBemComSaude', new CloackerControllerLegacy().viverBemComSaude)
 
