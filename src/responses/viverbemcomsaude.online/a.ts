5c6ed8d68747444d9038c6a673d9cf9803137492
edsonpag <edsonsilvapagani@gmail.com>
2024-07-05 22:20:34 -0300

    ok

--- src/responses/viverbemcomsaude.online/a.ts
+++ src/responses/viverbemcomsaude.online/a.ts
@@ -2,9 +2,9 @@ import { CloackerResponseLegacy } from "../../interfaces/cloakerResponseLegacy.i
 
 export const responseAViverBemComSaude: CloackerResponseLegacy = {
     headline: `<h2>Juste ces <span class="green-highlight">3 ingrédients</span> dans de l'eau pendant 2 jours, et je me suis réveillé sans aucun problème de vision... <span class="green-highlight">( regarde ci-dessous !)</span></h2>`,
-    vturb: `<div id="vid_66834b6585e2fa000b0a69ed" style="position:relative;width:100%;padding: 56.20608899297424% 0 0;"> <img id="thumb_66834b6585e2fa000b0a69ed" src="https://images.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66834b6585e2fa000b0a69ed/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"> <div id="backdrop_66834b6585e2fa000b0a69ed" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div> </div> <style> .elementor-element:has(#smartplayer) { width: 100%; } </style>`,
-    vturbScriptId: 'scr_66834b6585e2fa000b0a69ed',
-    vturbScriptSrc: 'var s=document.createElement("script"); s.src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66834b6585e2fa000b0a69ed/player.js", s.async=!0,document.head.appendChild(s);',
+    vturb: `<div id="vid_6678c5bd44d9b4000b6fadd8" style="position:relative;width:100%;padding: 56.20608899297424% 0 0;"> <img id="thumb_6678c5bd44d9b4000b6fadd8" src="https://images.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/6678c5bd44d9b4000b6fadd8/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"> <div id="backdrop_6678c5bd44d9b4000b6fadd8" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div> </div>  <style> .elementor-element:has(#smartplayer) { width: 100%; } </style>`,
+    vturbScriptId: 'scr_6678c5bd44d9b4000b6fadd8',
+    vturbScriptSrc: 'var s=document.createElement("script"); s.src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/6678c5bd44d9b4000b6fadd8/player.js", s.async=!0,document.head.appendChild(s);',
     customScript: `(function () {
         var xhr = new XMLHttpRequest()
         xhr.onreadystatechange = function () {
@@ -32,8 +32,8 @@ export const responseAViverBemComSaude: CloackerResponseLegacy = {
                 location.href = '/downsell/downsell01/' + queryParams + '|downsell_01'
         })
 
-        const vturbId = '66834b6585e2fa000b0a69ed'
-        const timeToReleaseTheDownsellInSeconds = 910
+        const vturbId = '6678c5bd44d9b4000b6fadd8'
+        const timeToReleaseTheDownsellInSeconds = 908
         const myInterval = setInterval(() => {
             const currentVslTimeInSeconds = localStorage.getItem(vturbId)
             if (currentVslTimeInSeconds) {
@@ -60,8 +60,8 @@ export const responseAViverBemComSaude: CloackerResponseLegacy = {
                 location.href = '/downsell/downsell01/' + queryParams + '|downsell_01'
         })
 
-        const vturbId = '66834b6585e2fa000b0a69ed'
-        const timeToReleaseTheDownsellInSeconds = 910
+        const vturbId = '6678c5bd44d9b4000b6fadd8'
+        const timeToReleaseTheDownsellInSeconds = 908
         const myInterval = setInterval(() => {
             const currentVslTimeInSeconds = localStorage.getItem(vturbId)
             if (currentVslTimeInSeconds) {
