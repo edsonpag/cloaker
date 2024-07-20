8ce93461fb138a19bd73fecf46d4cf3ed0cded7c
edsonpag <edsonsilvapagani@gmail.com>
2024-07-19 23:06:02 -0300

    ok

--- src/responses/viverbemcomsaude.online/a.ts
+++ src/responses/viverbemcomsaude.online/a.ts
@@ -2,9 +2,9 @@ import { CloackerResponseLegacy } from "../../interfaces/cloakerResponseLegacy.i
 
 export const responseAViverBemComSaude: CloackerResponseLegacy = {
     headline: `<h2>Juste ces <span class="green-highlight">3 ingrédients</span> dans de l'eau pendant 2 jours, et je me suis réveillé sans aucun problème de vision... <span class="green-highlight">( regarde ci-dessous !)</span></h2>`,
-    vturb: `<div id="vid_66908882fed57d000b958bd7" style="position:relative;width:100%;padding: 56.20608899297424% 0 0;"> <img id="thumb_66908882fed57d000b958bd7" src="https://images.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66908882fed57d000b958bd7/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"> <div id="backdrop_66908882fed57d000b958bd7" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div> </div>  <style> .elementor-element:has(#smartplayer) { width: 100%; } </style>`,
-    vturbScriptId: 'scr_66908882fed57d000b958bd7',
-    vturbScriptSrc: 'var s=document.createElement("script"); s.src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66908882fed57d000b958bd7/player.js", s.async=!0,document.head.appendChild(s);',
+    vturb: `<div id="vid_669b19312d16c7000b1f4873" style="position:relative;width:100%;padding: 56.20608899297424% 0 0;"> <img id="thumb_669b19312d16c7000b1f4873" src="https://images.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/669b19312d16c7000b1f4873/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"> <div id="backdrop_669b19312d16c7000b1f4873" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div> </div> <style> .elementor-element:has(#smartplayer) { width: 100%; } </style>`,
+    vturbScriptId: 'scr_669b19312d16c7000b1f4873',
+    vturbScriptSrc: 'var s=document.createElement("script"); s.src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/669b19312d16c7000b1f4873/player.js", s.async=!0,document.head.appendChild(s);',
     customScript: `(function () {
         var xhr = new XMLHttpRequest()
         xhr.onreadystatechange = function () {
