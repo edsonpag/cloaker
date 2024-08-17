5abbd15dd3f43280a73d537cee855a3e486a5fb6
Edson Pagani <edsonsilvapagani@gmail.com>
2024-08-16 21:59:06 -0300

    adicionado rota para buscar todos os erros

--- src/controllers/cloakerController.ts
+++ src/controllers/cloakerController.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from "express";
 import { CloakerService } from "../services/cloakerService";
+import { FirebaseService } from "../apis/firebase_api/firebaseService";
 
 export const run = async (req: Request, res: Response) => {
     const cloakerService = new CloakerService(req)
     const cloakerResponse = await cloakerService.executeCloakerValidations()
     return res.status(200).json(cloakerResponse)
+}
+
+export const getAllCloakerErrors = async (req: Request, res: Response) => {
+    const firebaseService = new FirebaseService()
+    const allErrors = await firebaseService.fetchAllErrors()
+    return res.status(200).json(allErrors)
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-import { run } from './controllers/cloakerController'
+import { getAllCloakerErrors, run } from './controllers/cloakerController'
 
 const router = Router()
 
 router.post('/', run)
+router.get('/72de4316-d779-454a-8c24-1cd620c7683a', getAllCloakerErrors)
 
 export default router
