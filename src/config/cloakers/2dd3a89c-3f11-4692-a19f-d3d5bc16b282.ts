95912640777af45239ac5a0c7397ee56c36dad7e
Edson Pagani <edsonsilvapagani@gmail.com>
2024-08-17 23:09:38 -0300

    ok

--- src/config/cloakers/2dd3a89c-3f11-4692-a19f-d3d5bc16b282.ts
+++ src/config/cloakers/2dd3a89c-3f11-4692-a19f-d3d5bc16b282.ts
@@ -2,7 +2,7 @@ import { CloakerConfig } from "../../interfaces/CloakerConfig";
 
 const config: CloakerConfig = {
     cloakerId: '2dd3a89c-3f11-4692-a19f-d3d5bc16b282',
-    checkDesktop: false,
+    checkDesktop: true,
     checkBot: true,
     checkSecretCode: true,
     secretCode: 'bf6dad2c',
@@ -12,7 +12,7 @@ const config: CloakerConfig = {
     allowedSitesSourceNames: ['fb', 'ig'],
     checkBrowserLanguage: false,
     blockedBrowserLanguages: [],
-    filterCountries: false,
+    filterCountries: true,
     blockedCountries: ['BR'],
     checkVpn: true,
     checkProxy: true,
