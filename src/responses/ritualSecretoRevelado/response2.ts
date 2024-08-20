76640272bfbfb7f6f182c1f553226e82826e6aa2
Edson Pagani <edsonsilvapagani@gmail.com>
2024-08-20 03:59:00 -0300

    reduzido mais o retorno do cloaker

--- src/responses/ritualSecretoRevelado/response2.ts
+++ src/responses/ritualSecretoRevelado/response2.ts
@@ -4,6 +4,6 @@ export const response2: CloakerResponse = {
     vsl: `<div class="vturb-container"><div id="vid_66c02f9dda5246000c28ed24" style="position:relative;width:100%;padding: 56.25% 0 0;"> <img id="thumb_66c02f9dda5246000c28ed24" src="https://images.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66c02f9dda5246000c28ed24/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"> <div id="backdrop_66c02f9dda5246000c28ed24" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div> </div>  <style> .elementor-element:has(#smartplayer) { width: 100%; } </style></div>`,
     vturbScriptId: `scr_66c02f9dda5246000c28ed24`,
     vturbScript: `var s=document.createElement("script"); s.src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66c02f9dda5246000c28ed24/player.js", s.async=!0,document.head.appendChild(s);`,
-    headline: `<div class="headline"><p class="headline-text">Escute essa frequência sonora para atrair coisas boas em sua vida</p></div>`,
+    headline: `Escute essa frequência sonora para atrair coisas boas em sua vida`,
     customScript: `console.log("ok")`
 }
