d09cd3be63dba39cc0fc576464d8df037d67fa69
Edson Pagani <edsonsilvapagani@gmail.com>
2024-08-20 04:06:36 -0300

    ok

--- src/responses/ritualSecretoRevelado/response0.ts
+++ src/responses/ritualSecretoRevelado/response0.ts
@@ -1,7 +1,6 @@
 import { CloakerResponse } from "../../interfaces/CloakerResponse";
 
 export const response0: CloakerResponse = {
-
     vsl: `<div id="vid_66be33897036e1000bbf0f0e" style="position:relative;width:100%;padding: 100% 0 0;"> <img id="thumb_66be33897036e1000bbf0f0e" src="https://images.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66be33897036e1000bbf0f0e/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"> <div id="backdrop_66be33897036e1000bbf0f0e" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div> </div>  <style> .elementor-element:has(#smartplayer) { width: 100%; } </style>`,
     vturbScriptId: `scr_66be33897036e1000bbf0f0e`,
     vturbScript: `var s=document.createElement("script"); s.src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66be33897036e1000bbf0f0e/player.js", s.async=!0,document.head.appendChild(s);`,
--- src/responses/ritualSecretoRevelado/response1.ts
+++ src/responses/ritualSecretoRevelado/response1.ts
@@ -1,7 +1,6 @@
 import { CloakerResponse } from "../../interfaces/CloakerResponse";
 
 export const response1: CloakerResponse = {
-    
     vsl: `<div class="vturb-container"><div id="vid_66c02fe277838e000c71ea24" style="position:relative;width:100%;padding: 56.13305613305614% 0 0;"> <img id="thumb_66c02fe277838e000c71ea24" src="https://images.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66c02fe277838e000c71ea24/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"> <div id="backdrop_66c02fe277838e000c71ea24" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div> </div>  <style> .elementor-element:has(#smartplayer) { width: 100%; } </style></div>`,
     vturbScriptId: `scr_66c02fe277838e000c71ea24`,
     vturbScript: `var s=document.createElement("script"); s.src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66c02fe277838e000c71ea24/player.js", s.async=!0,document.head.appendChild(s);`,
