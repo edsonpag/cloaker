e80d5d7f2aeac8bafd187feabf3b1c6818d41b30
Edson Pagani <edsonsilvapagani@gmail.com>
2024-08-21 01:18:19 -0300

    ok

--- src/controllers/cloakerController.ts
+++ src/controllers/cloakerController.ts
@@ -4,7 +4,7 @@ import { CloakerConfig } from "../interfaces/CloakerConfig";
 
 export const ritualSecretoRevelado = async (req: Request, res: Response) => {
     const cloakerConfig: CloakerConfig = {
-        checkDesktop: true,
+        checkDesktop: false,
         checkBot: true,
         checkSecretCode: true,
         secretCode: 'bf6dad2c',
@@ -14,7 +14,7 @@ export const ritualSecretoRevelado = async (req: Request, res: Response) => {
         allowedSitesSourceNames: ['fb', 'ig'],
         checkBrowserLanguage: false,
         blockedBrowserLanguages: [],
-        filterCountries: true,
+        filterCountries: false,
         blockedCountries: ['BR'],
         checkVpn: true,
         checkProxy: true,
