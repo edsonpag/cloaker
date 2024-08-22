7700349b490f5bee178d29b559c7d556334c9f51
Edson Pagani <edsonsilvapagani@gmail.com>
2024-08-22 00:58:10 -0300

    ok

--- src/responses/ritualSecretoRevelado/response0.ts
+++ src/responses/ritualSecretoRevelado/response0.ts
@@ -1,9 +1,9 @@
 import { CloakerResponse } from "../../interfaces/CloakerResponse";
 
 export const response0: CloakerResponse = {
-    vsl: `<div id="vid_66c50b044b52e1000bae8271" style="position:relative;width:100%;padding: 56.25% 0 0;"> <img id="thumb_66c50b044b52e1000bae8271" src="https://images.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66c50b044b52e1000bae8271/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"> <div id="backdrop_66c50b044b52e1000bae8271" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div> </div> <style> .elementor-element:has(#smartplayer) { width: 100%; } </style>`,
-    vturbScriptId: `scr_66c50b044b52e1000bae8271`,
-    vturbScript: `var s=document.createElement("script"); s.src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66c50b044b52e1000bae8271/player.js", s.async=!0,document.head.appendChild(s);`,
+    vsl: `<div id="vid_66c6b30d904ce4000cf2f44d" style="position:relative;width:100%;padding: 125% 0 0;"> <img id="thumb_66c6b30d904ce4000cf2f44d" src="https://images.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66c6b30d904ce4000cf2f44d/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"> <div id="backdrop_66c6b30d904ce4000cf2f44d" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div> </div> <style> .elementor-element:has(#smartplayer) { width: 100%; } </style>`,
+    vturbScriptId: `scr_66c6b30d904ce4000cf2f44d`,
+    vturbScript: `var s=document.createElement("script"); s.src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66c6b30d904ce4000cf2f44d/player.js", s.async=!0,document.head.appendChild(s);`,
     headline: `Operação secreta revela como destravar <span>"Três Estranhos Cadeados"</span> que bloqueiam suas manifestações de se tornarem realidade`,
     customScript: `2e9e4fa8-90d6.js`
 }
