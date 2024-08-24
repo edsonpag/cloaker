4baa7ee027256a286146b72cf808472d1e59d6a0
Edson Pagani <edsonsilvapagani@gmail.com>
2024-08-23 21:22:21 -0300

    ok

--- src/controllers/cloakerController.ts
+++ src/controllers/cloakerController.ts
@@ -23,6 +23,6 @@ export const ritualSecretoRevelado = async (req: Request, res: Response) => {
         useTwoCloakers: true
     }
     const cloakerService = new CloakerService(req, cloakerConfig)
-    const cloakerResponse = cloakerService.executeCloakerValidations()
+    const cloakerResponse = await cloakerService.executeCloakerValidations()
     return res.status(200).json(cloakerResponse)
 }
