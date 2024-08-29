90ac0532dd52746c28b29736b2c4913f6cf5b432
Edson Pagani <edsonsilvapagani@gmail.com>
2024-08-29 01:11:49 -0300

    ok

--- src/responses/viverbemcomsaude.online/b.ts
+++ src/responses/viverbemcomsaude.online/b.ts
@@ -1,9 +1,9 @@
 import { CloakerResponse } from "../../interfaces/cloakerResponse.interface";
 
 export const response1: CloakerResponse = {
-    vsl: `<div id="vid_66c02fe277838e000c71ea24" style="position:relative;width:100%;padding: 56.13305613305614% 0 0;"> <img id="thumb_66c02fe277838e000c71ea24" src="https://images.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66c02fe277838e000c71ea24/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"> <div id="backdrop_66c02fe277838e000c71ea24" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div> </div>  <style> .elementor-element:has(#smartplayer) { width: 100%; } </style>`,
-    vturbScriptId: `scr_66c02fe277838e000c71ea24`,
-    vturbScript: `var s=document.createElement("script"); s.src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66c02fe277838e000c71ea24/player.js", s.async=!0,document.head.appendChild(s);`,
-    headline: `URGENTE: Essa frequência sonora é capaz de atrair dinheiro para você`,
+    vsl: `<div id="vid_66cfe2098e71cb000b70aca8" style="position:relative;width:100%;padding: 56.16224648985959% 0 0;"> <img id="thumb_66cfe2098e71cb000b70aca8" src="https://images.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66cfe2098e71cb000b70aca8/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"> <div id="backdrop_66cfe2098e71cb000b70aca8" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div> </div> <style> .elementor-element:has(#smartplayer) { width: 100%; } </style>`,
+    vturbScriptId: `scr_66cfe2098e71cb000b70aca8`,
+    vturbScript: `var s=document.createElement("script"); s.src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66cfe2098e71cb000b70aca8/player.js", s.async=!0,document.head.appendChild(s);`,
+    headline: `URGENT : Cette fréquence sonore est capable d’attirer de l’argent vers vous`,
     customScript: `53b445c4-3b79.js`
 }
