2e09ff2cd85bcfe7c9b43b31823f3ce83b58ee25
Edson Pagani <edsonsilvapagani@gmail.com>
2024-08-30 23:45:47 -0300

    ok

--- src/routes.ts
+++ src/routes.ts
@@ -4,5 +4,6 @@ import { CloackerController } from './controllers/cloackerController';
 const router = Router()
 
 router.post('/ritualsecretorevelado', new CloackerController().ritualsecretorevelado)
+router.post('/buenaSalud', new CloackerController().buenaSalud)
 
 export default router
