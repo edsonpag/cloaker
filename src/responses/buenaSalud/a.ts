76a5892b4a57e5b202c1f7d5bbaccf8e65cd391b
Edson Pagani <edsonsilvapagani@gmail.com>
2024-09-18 00:41:44 -0300

    ok

--- src/responses/buenaSalud/a.ts
+++ src/responses/buenaSalud/a.ts
@@ -1,9 +1,9 @@
 import { CloakerResponse } from "../../interfaces/cloakerResponse.interface";
 
 export const buenaSalud0: CloakerResponse = {
-    vsl: `<div id="vid_66ea2686143ba5000cb7e1eb" style="position:relative;width:100%;padding: 56.25% 0 0;"> <img id="thumb_66ea2686143ba5000cb7e1eb" src="https://images.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66ea2686143ba5000cb7e1eb/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"> <div id="backdrop_66ea2686143ba5000cb7e1eb" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div> </div> <style> .elementor-element:has(#smartplayer) { width: 100%; } </style>`,
-    vturbScriptId: `scr_66ea2686143ba5000cb7e1eb`,
-    vturbScript: `var s=document.createElement("script"); s.src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66ea2686143ba5000cb7e1eb/player.js", s.async=!0,document.head.appendChild(s);`,
+    vsl: `<div id="vid_66ea47e19c86ed000b99a715" style="position:relative;width:100%;padding: 56.25% 0 0;"> <img id="thumb_66ea47e19c86ed000b99a715" src="https://images.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66ea47e19c86ed000b99a715/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;"> <div id="backdrop_66ea47e19c86ed000b99a715" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"></div> </div> <style> .elementor-element:has(#smartplayer) { width: 100%; } </style>`,
+    vturbScriptId: `scr_66ea47e19c86ed000b99a715`,
+    vturbScript: `var s=document.createElement("script"); s.src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66ea47e19c86ed000b99a715/player.js", s.async=!0,document.head.appendChild(s);`,
     headline: ``,
     customScript: `2e9e4fa8-90d6.js`
 }
