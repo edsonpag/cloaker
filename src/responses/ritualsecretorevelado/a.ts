9a24409ef76950e2b95d8b9bf61202cbcd760114
Edson Pagani <edsonsilvapagani@gmail.com>
2024-11-02 21:54:30 -0300

    ok

--- src/responses/ritualsecretorevelado/a.ts
+++ src/responses/ritualsecretorevelado/a.ts
@@ -1,6 +1,5 @@
 import { CloakerResponse } from "../../interfaces/cloakerResponse.interface";
 
 export const ritualSecretoRevelado0: CloakerResponse = {
-    l: 'https://eceducation.mycartpanda.com/checkout/151908219:1',
-    f: '2e9e4fa8-90d6.js'
+    l: 'https://eceducation.mycartpanda.com/checkout/151908219:1'
 }
--- src/responses/ritualsecretorevelado/b.ts
+++ src/responses/ritualsecretorevelado/b.ts
@@ -1,6 +1,5 @@
 import { CloakerResponse } from "../../interfaces/cloakerResponse.interface";
 
 export const ritualSecretoRevelado1: CloakerResponse = {
-    l: 'https://pay.hotmart.com/J95347186O?off=6ziq55wr&checkoutMode=10',
-    f: '53b445c4-3b79.js'
+    l: 'https://pay.hotmart.com/J95347186O?off=6ziq55wr&checkoutMode=10'
 }
