23452dbb5bbf305c364abffcc598208c060cc9bc
Edson Pagani <edsonsilvapagani@gmail.com>
2024-11-24 19:55:44 -0300

    ok

--- src/responses/ritualsecretorevelado/b.ts
+++ src/responses/ritualsecretorevelado/b.ts
@@ -1,5 +1,7 @@
 export const ritualSecretoRevelado1 = {
     headline: ``,
     vturb: ``,
+    vturbScriptId: ``,
+    vturbScript: ``,
     customContent: 'af354321.js'
 }
--- src/responses/ritualsecretorevelado/c.ts
+++ src/responses/ritualsecretorevelado/c.ts
@@ -1,5 +1,7 @@
 export const ritualSecretoRevelado2 = {
     headline: ``,
     vturb: ``,
+    vturbScriptId: ``,
+    vturbScript: ``,
     customContent: 'af354321.js'
 }
