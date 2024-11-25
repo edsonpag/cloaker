e549bc0c24dada35291cd081fd82ddba366b9ef9
Edson Pagani <edsonsilvapagani@gmail.com>
2024-11-24 22:26:06 -0300

    ok

--- src/controllers/cloackerController.ts
+++ src/controllers/cloackerController.ts
@@ -10,7 +10,7 @@ export class CloackerController {
     async ritualsecretorevelado(req: Request, res: Response) {
         const cloackerUtils = new CloackerUtils({
             checkDesktop: false,
-            validarParametrosDaUrl: false,
+            validarParametrosDaUrl: true,
             parametroParaValidar: '1de2f342',
             validarIp: false,
             paisesBloqueados: ['BR']
