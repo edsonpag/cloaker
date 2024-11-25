9c6be7175271693477df585c6165ab530e540a99
Edson Pagani <edsonsilvapagani@gmail.com>
2024-11-25 04:16:41 -0300

    ok

--- src/controllers/cloackerController.ts
+++ src/controllers/cloackerController.ts
@@ -12,7 +12,7 @@ export class CloackerController {
             checkDesktop: false,
             validarParametrosDaUrl: true,
             parametroParaValidar: '1de2f342',
-            validarIp: false,
+            validarIp: true,
             paisesBloqueados: ['BR']
         })
         cloackerUtils.validarPc(req)
