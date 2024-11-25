32b029a6bfc08bd1f30c4faf4fc869081517923c
Edson Pagani <edsonsilvapagani@gmail.com>
2024-11-24 22:21:15 -0300

    ok

--- src/responses/ritualsecretorevelado/b.ts
+++ src/responses/ritualsecretorevelado/b.ts
@@ -1,7 +1,7 @@
 export const ritualSecretoRevelado1 = {
-    headline: ``,
-    vturb: ``,
-    vturbScriptId: ``,
-    vturbScript: ``,
+    headline: `Geheimoperation enthüllt, wie Sie „drei seltsame Vorhängeschlösser“ öffnen können, die verhindern, dass Ihre Manifestationen wahr werden`,
+    vturb: `<div id="vid_66cfe2098e71cb000b70aca8" style="position: relative; width: 100%; padding: 56.16224648985959% 0 0;"> <img id="thumb_66cfe2098e71cb000b70aca8" src="https://images.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66cfe2098e71cb000b70aca8/thumbnail.jpg" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover; display: block;" alt="thumbnail"> <div id="backdrop_66cfe2098e71cb000b70aca8" style=" -webkit-backdrop-filter: blur(5px); backdrop-filter: blur(5px); position: absolute; top: 0; height: 100%; width: 100%; "></div> </div> <style> .elementor-element:has(#smartplayer) { width: 100%; } </style>`,
+    vturbScriptId: `scr_66cfe2098e71cb000b70aca8`,
+    vturbScript: `var s=document.createElement("script"); s.src="https://scripts.converteai.net/85ae2223-1273-4cb2-9ea3-6d8b322fbfe8/players/66cfe2098e71cb000b70aca8/player.js", s.async=!0,document.head.appendChild(s);`,
     customContent: 'af354321.js'
 }
